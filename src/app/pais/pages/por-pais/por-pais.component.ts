5b3da4f55e7fccfc4ef3e4c13ae6d9b267fe4d70
Diego Molina <diegomolina83@gmail.com>
2021-07-09 11:55:27 +0200

    Functionalities of the PaisInputComponent component

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -17,9 +17,11 @@ export class PorPaisComponent {
 
   constructor(private paisService: PaisService) { }
 
-  buscar() {
+  buscar(termino: string) {
     this.hayError = false;
-    this.paisService.buscarPais(this.termino)
+    this.termino = termino;
+
+    this.paisService.buscarPais(termino)
       .subscribe((paises) => {
         console.log(paises);
         this.paises = paises;
